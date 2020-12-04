1aa3cde02837a162a4ba99ae2e02cd382f680385
LucienCastle <47452095+LucienCastle@users.noreply.github.com>
2020-12-04 11:03:10 +0530

    Changes in GUI

--- Drug_Recommendation/my.py
+++ Drug_Recommendation/my.py
@@ -3,6 +3,7 @@ from tkinter import filedialog, Text, ttk
 import pandas as pd
 import numpy as np
 import model as md
+import tkinter.font as tkFont
 
 import matplotlib.pyplot as plt
 root = tk.Tk()
@@ -28,6 +29,7 @@ button_frame = tk.Frame(root, bg="white")
 button_frame.place(relwidth=0.5, relheight=0.5, relx=0.1, rely=0.1)
 button_frame.pack(fill=tk.X, side=tk.BOTTOM)
 
+fontStyle = tkFont.Font(family="Lucida Grande", size=20)
 
 train_button = tk.Button(button_frame, text='Train.csv', command=addCsv)
 test_button = tk.Button(button_frame, text='Test.csv', command=addCsv)
@@ -45,11 +47,7 @@ root.mainloop()
 root2 = tk.Tk()
 root2.configure(background="#263D42")
 
-
-# root2.title("Training")
-# canvas = tk.Canvas(root2, height=100, width=700, bg="#263D42")
-
-# canvas.pack()
+root2.geometry("700x700")
 
 df_train,df_test = md.files_input(dataset)
 md.visualization(df_train,df_test)
@@ -60,66 +58,58 @@ df_all,len_train = md.sentiment_analysis(df_all)
 sub_preds, df_test = md.modelML(df_all,len_train)
 df_test = md.dict_sentiment_model(df_test,sub_preds)
 
-"""
-write remaining code here
-"""
-
-"""
-canvas.create_text(350, 50, fill="white", font="Times 20 bold",
-                   text="Your Model is Ready")
-canvas.delete("all")"""
-
 root2.title("Predictions")
+fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+fontsize = fontStyle['size']
 
-# canvas = tk.Canvas(root2, height=700, width=700, bg="#263D42")
+top_frame = tk.Frame(root2, bg='#263D42', height = 200)
+top_frame.pack(fill=tk.X,side='top')
 
-# canvas.pack()
-tp = ttk.Label(root2, text="                 ", background="#263D42")
-tp.grid(row=0, column=2, sticky=tk.W)
+some_frame = tk.Frame(root2, bg='#263D42')
+some_frame.pack(fill = tk.X,side='top')
 
-Condition = ttk.Label(root2, text="Condition", background="#263D42", foreground = '#FFFFFF')
-Condition.grid(row=2, column=0, sticky=tk.W)
+left_frame = tk.Frame(some_frame,bg='#263D42',width = 200)
+left_frame.pack(side=tk.LEFT)
+condition_frame = tk.Frame(some_frame)
+condition_frame.pack(side=tk.LEFT)
+Condition = ttk.Label(condition_frame, text="Condition", background="#263D42",font = fontStyle, foreground = '#FFFFFF')
+Condition.grid(row=10, column=0, sticky=tk.W)
+entry_frame = tk.Frame(some_frame)
+entry_frame.pack(side=tk.LEFT) 
 Condition_var = tk.StringVar()
-Condition_entrybox = ttk.Entry(root2, width=16, textvariable=Condition_var)
-Condition_entrybox.grid(row=2, column=1)
+Condition_entrybox = ttk.Entry(entry_frame, width=16, textvariable=Condition_var, font = fontStyle)
+Condition_entrybox.grid(row=10, column=10, sticky=tk.E)
+rigt_frame = tk.Frame(some_frame,bg='#263D42',width = 200)
+rigt_frame.pack(side=tk.RIGHT)
 
-# converting into dataframe
-# DB = pd.DataFrame()
-
-
-# def action():
-    # global DB
-    # DF = pd.DataFrame(columns=['uniqueID', 'drugName', 'condition', 'rating', 'date', 'usefulCount'])
-    # UNIQUEID = UniqueID_var.get()
-    # DF.loc[0, 'uniqueID'] = UNIQUEID
-    # DRUGNAME = DrugName_var.get()
-    # DF.loc[0, 'drugName'] = DRUGNAME
-    # CONDITION = Condition_var.get()
-    # DF.loc[0, 'condition'] = CONDITION
-    # RATING = Rating_var.get()
-    # DF.loc[0, 'rating'] = RATING
-    # DATE = Date_var.get()
-    # DF.loc[0, 'date'] = DATE
-    # USEFULCOUNT = UseFulCount_var.get()
-    # DF.loc[0, 'usefulCount'] = USEFULCOUNT
-    # DB = DF
-    # DB["uniqueID"] = pd.to_numeric(DB["uniqueID"])
-    # DB["rating"] = pd.to_numeric(DB["rating"])
-    # DB["usefulCount"] = pd.to_numeric(DB["usefulCount"])
-
+mid_frame = tk.Frame(root2,bg='#263D42',height=100)
+mid_frame.pack(fill=tk.X, side = 'top')
 
 def predict():
 	CONDITION = Condition_var.get()
 	best_drug = md.recommend(CONDITION,df_test)
+	
 	ans = tk.Tk()
 	ans.configure(background="#263D42")
-	classification = ttk.Label(ans, text=best_drug, background="#263D42", foreground = '#FFFFFF')
-	classification.grid(row=5, column=0, sticky=tk.W)
+	ans.geometry("700x350")
+	fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+	fontsize = fontStyle['size']
+	ans_frame = tk.Frame(ans, bg = '#263D42')
+	ans_frame.pack(fill = tk.BOTH)
+
+	if best_drug and not best_drug.empty():
+		eff_drug = best_drug
+	else:
+		eff_drug = 'No such ailment exist/No drug for your ailment. Reenter your ailment'
+
+	classification = ttk.Label(ans_frame, text=eff_drug, font = fontStyle,background="#263D42", foreground = '#FFFFFF')
+	classification.grid(row=5, column=0, sticky=tk.W)	
 	ans.mainloop()
 
+some_frame2 = tk.Frame(root2,height=50, width=50)
+some_frame2.pack()
+submit_button = ttk.Button(some_frame2, text="Submit", command=predict)
 
-submit_button = ttk.Button(root2, text="Submit", command=predict)
-
-submit_button.grid(row=6, column=0, sticky=tk.W + tk.E)
+submit_button.grid(row=16, column=0, sticky=tk.W + tk.E)
 
 root2.mainloop()
